f75417435b12929b61a1c2a22f04de5270e10228
Lustihgn <quangtrannhat41@gmail.com>
2026-02-16 21:55:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-// ===== DANH SÁCH ẢNH CỦA BẠN =====
 const images = [
-  "anh1.jpg",
-  "anh2.jpg",
-  "anh3.jpg",
-  "anh4.jpg",
-  "anh5.jpg",
-  "anh6.jpg",
-  "anh7.jpg",
-  "anh8.jpg",
-  "anh9.jpg",
-  "anh10.jpg",
-  "anh11.jpg",
-  "anh12.jpg"
+  "anh1.jpg","anh2.jpg","anh3.jpg","anh4.jpg",
+  "anh5.jpg","anh6.jpg","anh7.jpg","anh8.jpg",
+  "anh9.jpg","anh10.jpg","anh11.jpg","anh12.jpg"
 ];
 
-function randomImage() {
-  return images[Math.floor(Math.random() * images.length)];
+let queue = [...images];
+let used = [];
+
+function getNextImage() {
+  if (queue.length === 0) {
+    queue = [...used];
+    used = [];
+  }
+  const img = queue.splice(Math.floor(Math.random() * queue.length), 1)[0];
+  used.push(img);
+  return img;
 }
 
-function createBalloon(side) {
+function createImage(side) {
   const img = document.createElement("img");
-  img.src = randomImage();
-  img.className = "balloon";
+  img.src = getNextImage();
+  img.className = "flying-img";
 
-  // Không sát rìa
-  const padding = 40;
-  const maxWidth = side.clientWidth - 160;
-  img.style.left = padding + Math.random() * maxWidth + "px";
-
-  // Bay chậm – mượt
-  const duration = 12 + Math.random() * 4;
-  img.style.animationDuration = duration + "s";
+  img.style.left = Math.random() * 60 + "%";
 
   side.appendChild(img);
 
-  // CHỈ XÓA KHI ANIMATION KẾT THÚC
   img.addEventListener("animationend", () => {
     img.remove();
   });
 }
 
-const leftSide = document.querySelector(".left");
-const rightSide = document.querySelector(".right");
-
-// TẦN SUẤT THẤP – ỔN ĐỊNH
 setInterval(() => {
-  createBalloon(leftSide);
-  createBalloon(rightSide);
-}, 3500);
+  createImage(document.querySelector(".left"));
+  createImage(document.querySelector(".right"));
+}, 2000);
